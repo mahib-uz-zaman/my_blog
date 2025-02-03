4a2948338f11645aad70514906dd3b8c98174c56
Mahib Uz Zaman <99533266+mahib-uz-zaman@users.noreply.github.com>
2025-02-03 17:26:53 +0600

    Add files via upload

--- quickbytes/blog/views.py
+++ quickbytes/blog/views.py
@@ -6,13 +6,23 @@ from django.views.generic import CreateView
 from .models import Post, Comment, Like
 from .forms import PostForm, CommentForm
 
+from django.shortcuts import render
+from .models import Post, Like
+
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
     for post in posts:
         post.like_count = post.likes.filter(is_dislike=False).count()  # Number of likes
         post.dislike_count = post.likes.filter(is_dislike=True).count()  # Number of dislikes
+        # Check if the current user has liked or disliked the post
+        if request.user.is_authenticated:
+            user_like = post.likes.filter(user=request.user).first()
+            post.user_has_liked = user_like and not user_like.is_dislike
+            post.user_has_disliked = user_like and user_like.is_dislike
+        else:
+            post.user_has_liked = False
+            post.user_has_disliked = False
     return render(request, 'blog/home.html', {'posts': posts})
-
 @login_required
 def create_post(request):
     if request.method == 'POST':
@@ -29,6 +39,13 @@ def create_post(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = post.comments.filter(parent=None)  # Only top-level comments
+    if request.user.is_authenticated:
+        user_like = post.likes.filter(user=request.user).first()
+        post.user_has_liked = user_like and not user_like.is_dislike
+        post.user_has_disliked = user_like and user_like.is_dislike
+    else:
+        post.user_has_liked = False
+        post.user_has_disliked = False
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -40,7 +57,6 @@ def post_detail(request, post_id):
     else:
         form = CommentForm()
     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
-
 @login_required
 def like_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
